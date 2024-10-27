65925a2728399307bfe4f460d419367a7c7ef7f1
Guilherme <guilhermesantosalves068@gmail.com>
2024-10-27 16:50:10 -0300

    Projeto inicializado

---
+++ main.js
@@ -0,0 +1,11 @@
+function teste() {
+    alert("Você clicou no apu!!!");
+}
+
+function testeCup() {
+    alert("Você clicou no copo de squishee!!!");
+}
+
+function zapera() {
+    alert("Oi Isaias!!!");
+}
